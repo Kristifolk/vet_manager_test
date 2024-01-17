36e79046c0ff8852ddaa32ca5ef5621f3ad788a6
kristin <devkristin.karr@gmail.com>
2024-01-17 10:09:46 +0300

    refactoring task_18

--- task_18.php
+++ task_18.php
@@ -1,3 +1,6 @@
+/*
+https://onlinephp.io/c/7ab6a
+*/
 <?php
 // ### Исключения  (6 часов)
 /* 18.
@@ -33,19 +36,37 @@ try {
 function customthrow(int $i)
 {
     if ($i < 6) {
-        throw new Exception("Exception customthrow");
+        throw new \Exception("Value must be greater than or equal to 6");
     }
 }
 
-// Написать свою реализацию функции  assertException(стандартными средствами языка php), которая будет проверять что вызывается нужное исключение
-function assertException()
+// Написать свою реализацию функции assertException, которая будет проверять что вызывается нужное исключение
+//передаем имя класса исключения и функцию в качестве аргумента
+function assertException(string $exceptionClass, callable $callback)
 {
-
+    try {
+        $callback();// Вызов функции или метода, переданного в качестве аргумента
+        echo "passed";
+    } catch (\Exception $e) {
+        // get_class для получения имени класса объекта. Она принимает объект в качестве аргумента и возвращает строку с именем класса этого объекта
+        if (get_class($e) === $exceptionClass) {//для получения имени класса объекта исключения, которое было поймано и сравниваю с тем,которое ожидалось  в аргументах функции assertException
+            echo "exception: " . $e->getMessage(), "\n";
+        } else {
+            throw $e;
+        }
+    }
 }
 
 assertException(
-    \Exception::class,
-    function () {
+    \Exception::class,//имя класса исключения
+    function () {//ф-я
         customthrow(2);
+    }
+);
+
+assertException(
+    \Exception::class,//имя класса исключения
+    function () {//ф-я
+        customthrow(9);
     }
 );
